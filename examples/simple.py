9cfca6af42eb86deb9e9b34fbb17f4bb1deb2a09
Kyle Conroy <kyleconroy@gmail.com>
2017-06-18 11:23:09 -0400

    code cleanup

--- examples/simple.py
+++ examples/simple.py
@@ -1,3 +1,11 @@
+# just for comparisons, let's import the originals from astropy
+import astropy.constants as c_astropy
+import astropy.units as u_astropy
+
+print "from astropy (before unitsiau2015 import) G =", c_astropy.G.value
+
 from unitsiau2015 import u, c
 
-print c.c.value
+# once unitsiau2015 is imported, the values within astropy have also changed
+print "from astropy (after unitsiau2015 import) G =", c_astropy.G.value
+print "from unitsiau2015 G =", c.G.value
