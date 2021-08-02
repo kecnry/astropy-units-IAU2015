8d6cd47138e8a34b2e99f1826d64416f991615b9
Kyle <kyleconroy@gmail.com>
2021-08-02 10:23:05 -0500

    fix _get_physical_type for astropy updates
    * now need to call str around the returned physical type object

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='unitsiau2015',
-      version='0.1.0',
+      version='0.1.1',
       description='Hack astropy units to adhere to IAU 2015 resolution',
       author='Kyle Conroy',
       author_email='kyleconroy@gmail.com',
